9513406fef9d7a7c148de48b267dc07a68874ece
Kelson Wysocki <32180124+Kotuon@users.noreply.github.com>
2023-11-21 08:57:47 -0800

    Working more on animations

--- src/Player.gd
+++ src/Player.gd
@@ -6,14 +6,14 @@ extends CharacterBody2D
 @export var max_walk_speed = 350
 @export var walk_acceleration = 600
 @export var brake_speed = 1250
-@export var turn_speed = 1000
+@export var turn_speed = 10000
 
 var current_walk_speed = 0
 var can_walk = true
 
 #Dash
-@export var time_to_dash = 0.1
-@export var dash_distance = 300
+@export var time_to_dash = 0.2
+@export var dash_distance = 200
 
 var dash_direction : Vector2
 var time_since_dash_started = 0.0
@@ -21,11 +21,13 @@ var is_dashing = false
 
 # Attack
 var attack_damage = 10
+var is_attacking = false
 
 var can_take_damage = true
 
 # Animations
-@onready var animated_sprite = $AnimatedSprite2D
+@onready var animation_player = $AnimationPlayer
+@onready var sprite = $Sprite2D
 
 func _draw():
     pass
@@ -33,7 +35,7 @@ func _draw():
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    animated_sprite.play("front_idle")
+    animation_player.play("front_idle")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -45,8 +47,11 @@ func _input(event: InputEvent):
         start_dash()
     
     if event.is_action_pressed("attack"):
-        #look_at(get_viewport().get_mouse_position())
-        pass
+        can_walk = false
+        is_attacking = true
+
+        var animation_direction = get_animation_direction(velocity)
+        animation_player.play(animation_direction + "_slash_first")
 
 func update_current_walk_speed(delta, direction : Vector2):
     if direction.length_squared() > 0.0:
@@ -61,34 +66,34 @@ func update_current_walk_speed(delta, direction : Vector2):
             current_walk_speed = 0.0
 
 func get_animation_direction(direction : Vector2) -> StringName:
-    var animation_direction : StringName
+    var animation_direction = "front"
 
     if direction.y > 0.0:
         if direction.x == 0.0:
             animation_direction = "front"
         else:
             if direction.x > 0.0:
-                animated_sprite.flip_h = false
+                sprite.flip_h = false
                 animation_direction = "front_side"
             elif direction.x < 0.0:
-                animated_sprite.flip_h = true
+                sprite.flip_h = true
                 animation_direction = "front_side"
     elif direction.y < 0.0:
         if direction.x == 0.0:
             animation_direction = "back"
         else:
             if direction.x > 0.0:
-                animated_sprite.flip_h = false
+                sprite.flip_h = false
                 animation_direction = "back_side"
             elif direction.x < 0.0:
-                animated_sprite.flip_h = true
+                sprite.flip_h = true
                 animation_direction = "back_side"
     else:
         if direction.x > 0.0:
-            animated_sprite.flip_h = false
+            sprite.flip_h = false
             animation_direction = "side"
         elif direction.x < 0.0:
-            animated_sprite.flip_h = true
+            sprite.flip_h = true
             animation_direction = "side"
 
     return animation_direction
@@ -107,9 +112,11 @@ func walk(delta):
     var animation_direction = get_animation_direction(velocity)
 
     if current_walk_speed == 0.0:
-        animated_sprite.play("front_idle")
+        animation_player.play("front_idle")
+    elif current_walk_speed < 200:
+        animation_player.play(animation_direction + "_walk")
     else:
-        animated_sprite.play(animation_direction + "_run")
+        animation_player.play(animation_direction + "_run")
     move_and_slide()
 
 func start_dash():
@@ -119,7 +126,7 @@ func start_dash():
     time_since_dash_started = 0.0
     dash_direction = (get_viewport().get_mouse_position() - position).normalized()
 
-    animated_sprite.play(get_animation_direction(dash_direction) + "_dash")
+    animation_player.play(get_animation_direction(dash_direction) + "_dash")
 
     velocity = dash_direction * (dash_distance / time_to_dash)
 
@@ -135,3 +142,4 @@ func dash(delta):
         is_dashing = false
         can_take_damage = true
         velocity = Vector2.ZERO
+        current_walk_speed = 0.0
